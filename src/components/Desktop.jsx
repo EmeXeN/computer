aa71653b5ed99238d7bce8c56c41b4cbe3a170e0
EmeXeN <pawelek0129@o2.pl>
2022-10-13 15:49:16 +0200

    created top bar for desktop

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faVolumeLow, faChevronDown, faLanguage } from '@fortawesome/free-solid-svg-icons'
 
 export default function Desktop() {
   const ONE_SECOND_IN_MILISECONDS = 1000
@@ -19,6 +21,9 @@ export default function Desktop() {
   return (
     <div className='desktop'>
       <div className="topbar">
+        <FontAwesomeIcon icon={faLanguage} />
+        <FontAwesomeIcon icon={faVolumeLow} />
+        <FontAwesomeIcon icon={faChevronDown} />
         <time className="topbar__time">{currentTime}</time>
       </div>
     </div>
